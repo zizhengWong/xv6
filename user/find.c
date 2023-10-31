ef85cad9fd369ab259bbb832cae6d3e272c07cbc
zizheng02 <zizheng.wang02@gmail.com>
2023-10-31 09:09:12 +0000

    fix：给find添加注释

--- user/find.c
+++ user/find.c
@@ -32,14 +32,15 @@ void find(char *path, const char *filename)
     fprintf(2, "find: path too long\n");
     return;
   }
+  // printf("path: %s\n",path); //在终端看看path是什么样子的，就是目录的路径（相对路径）
   strcpy(buf, path);
-  p = buf + strlen(buf);
-  *p++ = '/'; //p指向最后一个'/'之后
+  p = buf + strlen(buf); //移动p指针
+  *p++ = '/'; //添加一个'/'
   while (read(fd, &de, sizeof de) == sizeof de) {
     if (de.inum == 0) //inode号为0通常是一个无效的值，表示一个未分配的inode或一个已删除的文件
       continue;
     memmove(p, de.name, DIRSIZ); //添加路径名称，将 de.name 中的数据复制到指针 p 指向的内存位置，复制的数据长度是 DIRSIZ 字节。
-    p[DIRSIZ] = 0;               //字符串结束标志，p原地偏移DIRSIZ
+    p[DIRSIZ] = 0;               //字符串结束标志，p原地偏移DIRSIZ的位置，但p不动，现在p相当于只有一个文件名
     if (stat(buf, &st) < 0) {
       fprintf(2, "find: cannot stat %s\n", buf);
       continue;
@@ -47,8 +48,10 @@ void find(char *path, const char *filename)
     //不要在“.”和“..”目录中递归
     if (st.type == T_DIR && strcmp(p, ".") != 0 && strcmp(p, "..") != 0) {
       find(buf, filename);
-    } else if (strcmp(filename, p) == 0)
+    } else if (strcmp(filename, p) == 0){
+      // printf("p: %s\n",p); //可以在终端看到文件名输出
       printf("%s\n", buf);
+    }
   }
 
   close(fd);
